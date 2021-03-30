08294e1aad1a58478188ec54e854ed3c513191ee
Dark1078 <Davidlau001007@gmail.com>
2021-03-29 23:10:12 -0400

    added embeds leave and join messages too

--- Main.py
+++ Main.py
@@ -1,15 +1,59 @@
+from sqlite3.dbapi2 import Cursor
 import discord
+from discord.flags import Intents
 from epicstore_api import *
 import urllib.request
 from discord.ext import commands
 import random
 import SecretCode
-client = commands.Bot(command_prefix='.')
+import sqlite3
+import datetime
+
+intents = discord.Intents().all()
+client = commands.Bot(command_prefix='.', intents=intents)
 
 
 @client.event
 async def on_ready():
+    # db = sqlite3.connect('main.sqlite')
+    # cursor = db.cursor()
+    # cursor.execute('''
+    # CREATE TABLE IF NOT EXISTS main(
+    #     guild_id TEXT,
+    #     msg TEXT,
+    #     channel_id TEXT
+    #     )
+    # ''')
     print("ArkNight is ready")
+    return await client.change_presence(activity=discord.Activity(type=1, name='ArkNight', url='https://www.twitch.tv/zoramx'))
+
+
+@client.event
+async def on_member_join(member):
+    embed = discord.Embed(
+        colour=0x56ff39, title=f"Welcome to the Quaz's server! There are now {len(list(member.guild.members))} members.")
+    embed.set_thumbnail(url=f"{member.avatar_url}")
+    embed.set_author(name=f"{member.name}",
+                     icon_url=f"{member.avatar_url}")
+    embed.set_footer(text=f"{member.guild}",
+                     icon_url=f"{member.guild.icon_url}")
+    embed.timestamp = datetime.datetime.utcnow()
+    channel = client.get_channel(id=818600857075187745)
+    await channel.send(embed=embed)
+
+
+@client.event
+async def on_member_remove(member):
+    embed = discord.Embed(
+        colour=0xff0303, title=f"{member.name} has left the Quaz's server, there are now {len(list(member.guild.members))} members.")
+    embed.set_thumbnail(url=f"{member.avatar_url}")
+    embed.set_author(name=f"{member.name}",
+                     icon_url=f"{member.avatar_url}")
+    embed.set_footer(text=f"{member.guild}",
+                     icon_url=f"{member.guild.icon_url}")
+    embed.timestamp = datetime.datetime.utcnow()
+    channel = client.get_channel(id=818600857075187745)
+    await channel.send(embed=embed)
 
 
 @client.command()
@@ -17,17 +61,19 @@ async def game(channel):
     file_name = "game_image"
     file_path = "images/"
     game = grab_free_game()
-    grab_image(file_path, file_name)
-    await channel.send(game)
-    await channel.send(file=discord.File(file_path+file_name+".jpg"))
+    image = grab_image()
+    embed = discord.Embed(
+        colour=0x56ff39, title=f"Current free game: {game}")
+    embed.set_image(url=(image))
+    await channel.send(embed=embed)
 
 
-@client.command()
+@ client.command()
 async def kick(channel, member: discord.Member, *, reason=None):
     await member.kick(reason=reason)
 
 
-@client.command()
+@ client.command()
 async def hentai(channel):
     code = ""
     for i in range(6):
@@ -35,12 +81,12 @@ async def hentai(channel):
     await channel.send(code)
 
 
-@client.command()
+@ client.command()
 async def ban(channel, member: discord.Member, *, reason=None):
     await member.ban(reason=reason)
 
 
-@client.command()
+@ client.command()
 async def purge(channel, count=1):
     await channel.channel.purge(limit=count)
 
@@ -51,7 +97,6 @@ def grab_free_game():
     for _ in free_games["data"]["Catalog"]["searchStore"]["elements"]:
         try:
             game = _["promotions"]["promotionalOffers"]
-            print(game)
         except:
             pass
         for i in game:
@@ -61,7 +106,7 @@ def grab_free_game():
                 return (_["title"])
 
 
-def grab_image(file_path, file_name):
+def grab_image():
     epic_api = EpicGamesStoreAPI()
     free_games = epic_api.get_free_games()
     for _ in free_games["data"]["Catalog"]["searchStore"]["elements"]:
@@ -73,8 +118,7 @@ def grab_image(file_path, file_name):
             if len(game) == 0:
                 continue
             else:
-                path = file_path + file_name + ".jpg"
-                urllib.request.urlretrieve(_["keyImages"][1]["url"], path)
+                return _["keyImages"][1]["url"]
 
 
 client.run(SecretCode.code)
