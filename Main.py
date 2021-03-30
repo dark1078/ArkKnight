3fc224646b798043bb5203f096d28dce404b416a
Dark1078 <Davidlau001007@gmail.com>
2021-03-30 16:57:51 -0400

    done

--- Main.py
+++ Main.py
@@ -13,6 +13,15 @@ intents = discord.Intents().all()
 client = commands.Bot(command_prefix='.', intents=intents)
 
 
+class Data:
+    def __init__(self):
+        self.welcome_channel = None
+        self.goodbye_channel = None
+
+
+data = Data()
+
+
 @client.event
 async def on_ready():
     # db = sqlite3.connect('main.sqlite')
@@ -30,30 +39,34 @@ async def on_ready():
 
 @client.event
 async def on_member_join(member):
-    embed = discord.Embed(
-        colour=0x56ff39, title=f"Welcome to the Quaz's server! There are now {len(list(member.guild.members))} members.")
-    embed.set_thumbnail(url=f"{member.avatar_url}")
-    embed.set_author(name=f"{member.name}",
-                     icon_url=f"{member.avatar_url}")
-    embed.set_footer(text=f"{member.guild}",
-                     icon_url=f"{member.guild.icon_url}")
-    embed.timestamp = datetime.datetime.utcnow()
-    channel = client.get_channel(id=818600857075187745)
-    await channel.send(embed=embed)
+    if data.welcome_channel != None:
+        embed = discord.Embed(
+            colour=0x56ff39, title=f"Welcome to the Quaz's server! There are now {len(list(member.guild.members))} members.")
+        embed.set_thumbnail(url=f"{member.avatar_url}")
+        embed.set_author(name=f"{member.name}",
+                         icon_url=f"{member.avatar_url}")
+        embed.set_footer(text=f"{member.guild}",
+                         icon_url=f"{member.guild.icon_url}")
+        embed.timestamp = datetime.datetime.utcnow()
+        await data.welcome_channel(embed=embed)
+    else:
+        print("Welcome channel not set")
 
 
 @client.event
 async def on_member_remove(member):
-    embed = discord.Embed(
-        colour=0xff0303, title=f"{member.name} has left the Quaz's server, there are now {len(list(member.guild.members))} members.")
-    embed.set_thumbnail(url=f"{member.avatar_url}")
-    embed.set_author(name=f"{member.name}",
-                     icon_url=f"{member.avatar_url}")
-    embed.set_footer(text=f"{member.guild}",
-                     icon_url=f"{member.guild.icon_url}")
-    embed.timestamp = datetime.datetime.utcnow()
-    channel = client.get_channel(id=818600857075187745)
-    await channel.send(embed=embed)
+    if data.welcome_channel != None:
+        embed = discord.Embed(
+            colour=0xff0303, title=f"{member.name} has left the Quaz's server, there are now {len(list(member.guild.members))} members.")
+        embed.set_thumbnail(url=f"{member.avatar_url}")
+        embed.set_author(name=f"{member.name}",
+                         icon_url=f"{member.avatar_url}")
+        embed.set_footer(text=f"{member.guild}",
+                         icon_url=f"{member.guild.icon_url}")
+        embed.timestamp = datetime.datetime.utcnow()
+        await data.welcome_channel.send(embed=embed)
+    else:
+        print("Goodbye channel not set")
 
 
 @client.command()
@@ -69,8 +82,34 @@ async def game(channel):
 
 
 @ client.command()
-async def kick(channel, member: discord.Member, *, reason=None):
+@ commands.has_permissions(administrator=True)
+async def set_welcome_channel(ctx, channel_name=None):
+    if channel_name != None:
+        for channel in ctx.guild.channels:
+            if channel.name == channel_name:
+                data.welcome_channel = channel
+                await ctx.channel.send(f"Welcome channel set to {channel.name}")
+    else:
+        await ctx.channel.send("Please include the name of the channel")
+
+
+@ client.command()
+@ commands.has_permissions(administrator=True)
+async def set_goodbye_channel(ctx, channel_name=None):
+    if channel_name != None:
+        for channel in ctx.guild.channels:
+            if channel.name == channel_name:
+                data.goodbye_channel = channel
+                await ctx.channel.send(f"Goodbye channel set to {channel.name}")
+    else:
+        await ctx.channel.send("Please include the name of the channel")
+
+
+@ client.command()
+@ commands.has_permissions(kick_members=True)
+async def kick(ctx, member: discord.Member, *, reason="No reason provided"):
     await member.kick(reason=reason)
+    await ctx.send(f"{member} has been kicked.")
 
 
 @ client.command()
@@ -82,13 +121,16 @@ async def hentai(channel):
 
 
 @ client.command()
-async def ban(channel, member: discord.Member, *, reason=None):
+@ commands.has_permissions(ban_members=True)
+async def ban(ctx, member: discord.Member, *, reason="No reason provided"):
     await member.ban(reason=reason)
+    await ctx.send(f"{member} has been banned.")
 
 
 @ client.command()
-async def purge(channel, count=1):
-    await channel.channel.purge(limit=count)
+@ commands.has_permissions(manage_messages=True)
+async def purge(ctx, count=1):
+    await ctx.channel.purge(limit=count)
 
 
 def grab_free_game():
